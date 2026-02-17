88864e83a1f6fe99bb89b5a8b99a8267d34b14d3
Diego Van Eggelpoel <di525097@ucf.edu>
2026-02-16 19:15:21 -0500

    Initial Build
    Basic build including very limited features:
        -Movement is somewhat functional (there is a minor bug which results in some sliding after input is let go)
        -Placeholder Stamina bar

---
+++ Assets/Scripts/Player/playerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private float BASE_SPEED = 5f;
+
+    [Header("Dash Settings")]
+    [SerializeField] private float maxStamina = 3f;
+    [SerializeField] private float staminaDrainRate = 2f;     // per second
+    [SerializeField] private float staminaRegenRate = 1f;     // per second
+    [SerializeField] private float dashMultiplier = 2f;
+    [SerializeField] private float dashCooldownTime = 3f;
+    [SerializeField] private Image staminaFill;
+    private Rigidbody2D rb;
+
+    [SerializeField] private float currentStamina;
+    private bool isOnCooldown = false;
+
+    [Header("Player Stats")]
+    [SerializeField] private int hp;
+    [SerializeField] private int attack;
+    [SerializeField] private int defense;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentStamina = maxStamina;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 dir = new Vector2(horizontal, vertical).normalized;
+
+        bool isDashing = Input.GetKey(KeyCode.LeftShift);
+
+        float speedToUse = BASE_SPEED;
+
+        // DASH LOGIC
+        if (isDashing && currentStamina > 0 && !isOnCooldown)
+        {
+            speedToUse *= dashMultiplier;
+
+            // Drain stamina over time
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+
+            if (currentStamina <= 0)
+            {
+                currentStamina = 0;
+                StartCoroutine(DashCooldown());
+            }
+        }
+        else
+        {
+            // Regen stamina if not cooling down
+            if (!isOnCooldown && currentStamina < maxStamina)
+            {
+                currentStamina += staminaRegenRate * Time.deltaTime;
+                currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+            }
+        }
+
+        staminaFill.fillAmount = currentStamina / maxStamina;
+
+        // Apply movement
+        if (dir.magnitude < 0.1f)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            rb.linearVelocity = dir * speedToUse;
+        }
+
+        // Flip sprite
+        if (horizontal > 0)
+        {
+            transform.rotation = new Quaternion(0, -1, 0, 0);
+        }
+        else if (horizontal < 0)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+
+    IEnumerator DashCooldown()
+    {
+        isOnCooldown = true;
+
+        yield return new WaitForSeconds(dashCooldownTime);
+
+        isOnCooldown = false;
+    }
+}
