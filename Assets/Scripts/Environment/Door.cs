a7694599d84004efe1de49d55979cd9f4a758ba2
Lucas Bianchi <lucasmbianchi@gmail.com>
2026-03-01 16:27:03 -0500

    Added Health & Stamina Bar ... Added 3 Rooms for Clubhouse Area ... Added Tilemaps ... Added Doors and Door Script

---
+++ Assets/Scripts/Environment/Door.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System;
+
+public class Door : MonoBehaviour
+{
+    [Header("Door Settings")]
+    [SerializeField] private bool horizontal = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (horizontal)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
